7de438a6cbead612286291830fce5fb20e5670b2
Francesca Lugay <chescalugay@gmail.com>
2024-06-15 04:39:27 +0800

    Activity 8

---
+++ app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -0,0 +1,26 @@
+
+package com.example.cupcake
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.cupcake.ui.theme.CupcakeTheme
+
+//FRANCESCA A. LUGAY
+//BSIT 22A3
+//MOBILE COMPUTING
+//JUNE 15, 2024
+//ACTIVITY 8
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            CupcakeTheme {
+                CupcakeApp()
+            }
+        }
+    }
+}
